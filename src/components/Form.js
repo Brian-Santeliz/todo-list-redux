0a234594dbc3f4796391d229a106a159ffa83355
Brian <blsvsanteliz@gmail.com>
2020-12-16 21:43:57 -0400

    Ad state form

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,17 +1,38 @@
-import React from "react";
+import { useState } from "react";
 
 const Form = () => {
+  const [form, setForm] = useState({
+    name: "",
+    description: "",
+  });
+  const handleChange = ({ target, value }) => {
+    setForm((prev) => {
+      return {
+        ...prev,
+        [target]: value,
+      };
+    });
+  };
   return (
     <div className="col-md-3">
       <div className="form">
         <form method="post">
           <div className="form-group">
             <label htmlFor="name">Name:</label>
-            <input type="text" name="name" id="name" className="form-control" />
+            <input
+              onChange={handleChange}
+              value={form.name}
+              type="text"
+              name="name"
+              id="name"
+              className="form-control"
+            />
           </div>
           <div className="form-group">
             <label htmlFor="description">Description:</label>
             <input
+              onChange={handleChange}
+              value={form.description}
               type="text"
               name="Description"
               id="description"
