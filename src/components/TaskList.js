c70425edb1efac0e3cf63937893a6822cd2a6e82
Brian <blsvsanteliz@gmail.com>
2020-12-17 05:27:56 -0400

    Add table

--- src/components/TaskList.js
+++ src/components/TaskList.js
@@ -3,7 +3,15 @@ import React from "react";
 const TaskList = () => {
   return (
     <div className="col-md-8 mt-2 p-2 text-center">
-      <h1>hola</h1>
+      <div className="d-flex justify-content-center">
+        <table className="table table-bordered">
+          <td>
+            <th>Name</th>
+            <th>Description</th>
+            <th>Price</th>
+          </td>
+        </table>
+      </div>
     </div>
   );
 };
