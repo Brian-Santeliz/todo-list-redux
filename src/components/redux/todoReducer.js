2d0aacd5b74090cf390a9c1d12c9ce0d43895adc
Brian <blsvsanteliz@gmail.com>
2020-12-28 01:30:08 -0400

    Add dispatch to delete a task

--- src/components/redux/todoReducer.js
+++ src/components/redux/todoReducer.js
@@ -3,8 +3,6 @@ import {
   ADD_TASK_ERROR,
   DELETE_TASK,
   DELETE_TASK_ERROR,
-  GET_TASK,
-  GET_TASK_ERROR,
 } from "./types";
 const initialState = {
   todos: JSON.parse(localStorage.getItem("todos")) || [],
@@ -15,6 +13,11 @@ export const todoReducer = (state = initialState, { type, payload }) => {
       return {
         ...state,
         todos: [...state.todos, payload],
+      };
+    case DELETE_TASK:
+      return {
+        ...state,
+        todos: state.todos.filter((todo) => todo.id !== payload),
       };
     default:
       return state;
--- src/components/redux/types.js
+++ src/components/redux/types.js
@@ -1,7 +1,5 @@
 export const ADD_TASK = "ADD_TASK";
-export const GET_TASK = "GET_TASK";
 export const DELETE_TASK = "DELETE_TASK";
 
 export const ADD_TASK_ERROR = "ADD_TASK_ERROR";
-export const GET_TASK_ERROR = "GET_TASK_ERROR";
 export const DELETE_TASK_ERROR = "DELETE_TASK_ERROR";
