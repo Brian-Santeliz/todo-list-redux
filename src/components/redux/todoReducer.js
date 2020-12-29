a67ca5924d1da368e67376a82bd632f34ea8ff6b
Brian <blsvsanteliz@gmail.com>
2020-12-29 19:18:06 -0400

    add error case

--- src/components/redux/todoReducer.js
+++ src/components/redux/todoReducer.js
@@ -6,18 +6,27 @@ import {
 } from "./types";
 const initialState = {
   todos: JSON.parse(localStorage.getItem("todos")) || [],
+  error: false,
 };
 export const todoReducer = (state = initialState, { type, payload }) => {
   switch (type) {
     case ADD_TASK:
       return {
         ...state,
+        error: false,
         todos: [...state.todos, payload],
       };
     case DELETE_TASK:
       return {
         ...state,
+        error: false,
         todos: state.todos.filter((todo) => todo.id !== payload),
+      };
+    case ADD_TASK_ERROR:
+    case DELETE_TASK_ERROR:
+      return {
+        ...state,
+        error: true,
       };
     default:
       return state;
