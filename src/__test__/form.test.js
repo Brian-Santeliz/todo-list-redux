ddca1600f33e5e6c771db209e8761b06edf9a5c0
Brian <blsvsanteliz@gmail.com>
2020-12-30 01:01:29 -0400

    Add testing to show alert error

--- src/__test__/form.test.js
+++ src/__test__/form.test.js
@@ -1,10 +1,12 @@
-import { render, screen } from "@testing-library/react";
-import "@testing-library/jest-dom/extend-expect";
-import Form from "../components/Form";
+import { render, screen, cleanup, fireEvent } from "@testing-library/react";
 import { Provider } from "react-redux";
 import { store } from "../components/redux/store";
+import Form from "../components/Form";
+import "@testing-library/jest-dom/extend-expect";
 
-test("Render <Form /> component, show a button and input", () => {
+afterEach(cleanup);
+
+test("Testing to <Form /> component", () => {
   render(
     <Provider store={store}>
       <Form />
@@ -12,14 +14,20 @@ test("Render <Form /> component, show a button and input", () => {
   );
   const botonSave = screen.getByTestId("boton");
   const inputTask = screen.getByTestId("inputTask");
-  const btnText = screen.getByText("Save");
 
   expect(botonSave).toBeInTheDocument();
-  expect(botonSave.tagName).toBe("BfUTTON");
+  expect(botonSave.tagName).toBe("BUTTON");
   expect(botonSave.tagName).not.toBe("INPUT");
-  expect(btnText).toBeInTheDocument();
-  expect(btnText.textContent).not.toBe("Guardar");
+  expect(botonSave.textContent).toBe("Save");
+  expect(botonSave.textContent).not.toBe("Guardar");
+
   expect(inputTask).toBeInTheDocument();
   expect(inputTask.tagName).toBe("INPUT");
-  expect(inputTask.tagName).not.toBe("button");
+  expect(inputTask.tagName).not.toBe("BUTTON");
+
+  fireEvent.click(botonSave);
+  const alertMessage = screen.getByTestId("alert");
+  expect(alertMessage.textContent).toBe("The task is required");
+  expect(alertMessage.textContent).not.toBe("The task is Required");
+  expect(alertMessage).toBeInTheDocument();
 });
