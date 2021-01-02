f7e541a0a84e4b0f289b8d8fd37b957dbfa45e26
Brian <blsvsanteliz@gmail.com>
2021-01-01 22:13:32 -0400

    react-hook-form aggregate

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -3,38 +3,14 @@ import { useDispatch } from "react-redux";
 import { useForm } from "react-hook-form";
 import { addTask } from "./redux/actions";
 const Form = () => {
-  const [form, setForm] = useState("");
-  const [errorForm, setErrorForm] = useState(false);
+  const [register, handleSubmit, errors] = useForm();
   const dispatch = useDispatch();
-  const validateForm = (formData) => {
-    if (formData.trim() === "") {
-      setErrorForm(true);
-      return true;
-    } else {
-      setErrorForm(false);
-      return false;
-    }
-  };
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    const isError = validateForm(form);
-    if (!isError) {
-      dispatch(addTask(form));
-    }
-    setForm("");
-  };
-  const errorMessage = errorForm && (
-    <div
-      className="alert alert-danger text-center text-capitalize"
-      data-testid="alert"
-    >
-      The task is required
-    </div>
-  );
+  // dispatch(addTask(form));
+
   return (
     <div className="col-md-4">
       <div className="form">
-        {errorMessage}
+        {/* {errorMessage} */}
         <form
           method="post"
           className="card card-body p-3 mt-3"
@@ -43,8 +19,6 @@ const Form = () => {
           <div className="form-group">
             <label htmlFor="name">Task:</label>
             <input
-              onChange={(e) => setForm(e.target.value)}
-              value={form}
               type="text"
               name="name"
               id="name"
@@ -52,6 +26,7 @@ const Form = () => {
               placeholder="Write a task..."
               autoFocus
               data-testid="inputTask"
+              ref={register}
             />
           </div>
           <button type="submit" className="btn btn-info" data-testid="boton">
