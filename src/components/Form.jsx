4e747b73cd272528bbffa2d472be7b621df57b19
Brian <blsvsanteliz@gmail.com>
2021-01-02 22:59:01 -0400

    change the logic of form with react-hook-form

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,20 +1,26 @@
-import { useState } from "react";
 import { useDispatch } from "react-redux";
 import { useForm } from "react-hook-form";
 import { addTask } from "./redux/actions";
 const Form = () => {
-  const [register, handleSubmit, errors] = useForm();
+  const { register, handleSubmit, errors, reset } = useForm();
   const dispatch = useDispatch();
-  // dispatch(addTask(form));
-
+  const onSubmitForm = ({ name }) => {
+    dispatch(addTask(name));
+    reset();
+  };
+  const error = errors.name?.message;
   return (
     <div className="col-md-4">
       <div className="form">
-        {/* {errorMessage} */}
+        {error && (
+          <p className="alert-danger p-2 text-center text-uppercase font-weight-bold">
+            {error}
+          </p>
+        )}
         <form
           method="post"
           className="card card-body p-3 mt-3"
-          onSubmit={handleSubmit}
+          onSubmit={handleSubmit(onSubmitForm)}
         >
           <div className="form-group">
             <label htmlFor="name">Task:</label>
@@ -26,7 +32,13 @@ const Form = () => {
               placeholder="Write a task..."
               autoFocus
               data-testid="inputTask"
-              ref={register}
+              ref={register({
+                required: "The task is required",
+                minLength: {
+                  value: 4,
+                  message: "Task have a min length of 4 characters",
+                },
+              })}
             />
           </div>
           <button type="submit" className="btn btn-info" data-testid="boton">
